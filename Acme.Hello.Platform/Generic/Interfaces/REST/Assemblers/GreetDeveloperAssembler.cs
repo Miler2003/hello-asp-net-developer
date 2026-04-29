7b0762397b15b1687cd19781522a316514faec03
Miler <mileralexander2003@gmail.com>
2026-04-29 00:49:25 -0500

    feat(developer-assembler): add greet developer assembler for converting developer entities to greet developer response objects.

---
+++ Acme.Hello.Platform/Generic/Interfaces/REST/Assemblers/GreetDeveloperAssembler.cs
@@ -0,0 +1,24 @@
+﻿using Acme.Hello.Platform.Generic.Domain.Model.Entities;
+using Acme.Hello.Platform.Generic.Interfaces.REST.Resources;
+
+namespace Acme.Hello.Platform.Generic.Interfaces.REST.Assemblers;
+
+/// <summary>
+///     Assembler class responsible for converting between Developer entities and GreetDeveloperResponse resources.
+/// </summary>
+public class GreetDeveloperAssembler
+{
+    /// <summary>
+    /// Converts a Developer entity into a GreetDeveloperResponse. If the entity is
+    /// it returns a response with a generic welcome message. Otherwise, it constr
+    /// </summary>
+    /// <param name="entity">The Developer entity to convert into GreetDeveloperResponse</param>
+    /// <returns>A GreetDeveloperResponse containing the developer's ID, fill name</returns>
+    public static GreetDeveloperResponse ToResponseFromEntity(Developer? entity)
+    {
+        if (entity is null || entity.IsAnyNameEmpty())
+            return new GreetDeveloperResponse("Welcome Anonymous ASP.NET Developer");
+        return new GreetDeveloperResponse(entity.Id, entity.GetFullName(),
+            $"Congrats {entity.GetFullName()}! You are an ASP.NET Developer");
+    }
+}
